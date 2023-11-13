e2a2eba2b2380d47844b10f7e8c36e50db880b96
안정민 <anjeongmin@anjeongmin-ui-MacBookAir.local>
2023-11-13 22:11:28 +0900

    [#9] add : pageTitle 컴포넌트 추가

---
+++ src/components/Atom/PageTitle/styled.js
@@ -0,0 +1,18 @@
+/*Header*/
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 100vw;
+  padding: 25px;
+  justify-content: center;
+  display: flex;
+  font-size: 32px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+  gap: 10px;
+`;
+
+export const Blue = styled.div`
+  color: #384a7d;
+`;
