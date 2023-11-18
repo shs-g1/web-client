3ab19369bd09bb23d287eab33ad4836767376ded
anjeongmin <102chimee@gmail.com>
2023-11-18 23:07:18 +0900

    [#22] add : 고객 컴포넌트 추가

--- src/components/index.js
+++ src/components/index.js
@@ -5,7 +5,10 @@ export { default as Calendar } from "./Atom/Calendar";
 export { default as TodoListItem } from "./Atom/TodoListItem";
 export { default as Table } from "./Atom/table";
 export { default as SlideButton } from "./Atom/slideButton";
+export { default as OptionButton } from "./Atom/optionButton";
 
 export { default as Header } from "./Molecule/Header";
 export { default as Todo } from "./Molecule/Todo";
 export { default as Incentive } from "./Molecule/Incentive";
+export { default as CustomerInfoContainer } from "./Molecule/customerInfoContainer";
+export { default as CustomerProfitContainer } from "./Molecule/customerProfitContainer";
