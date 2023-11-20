d04a5f8c490e46a06e1eea7d76330e62d2e5b517
anjeongmin <102chimee@gmail.com>
2023-11-20 21:36:11 +0900

    [#36] style : 모달 css 추가

--- src/components/Atom/table/styled.js
+++ src/components/Atom/table/styled.js
@@ -13,7 +13,6 @@ export const Style = styled.div`
     padding: 10px 20px;
     text-align: center;
     background-color: #fafafa;
-    border-radius: 6px;
     color: #b5b5c3;
     font-size: 16px;
     font-weight: 600;
@@ -25,8 +24,8 @@ export const Style = styled.div`
     color: #464e5f;
     font-size: 16px;
     font-weight: 600;
-    word-break: break-all; /* 텍스트가 셀을 넘어갈 경우 단어 단위로 줄 바꿈 */
-    max-width: 350px; /* 최대 너비를 지정 (원하는 값으로 조절) */
+    word-break: break-all;
+    max-width: 350px;
   }
 `;
 
