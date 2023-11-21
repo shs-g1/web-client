7caa67f8dbde51b7da5a4918282d426306d3ef72
anjeongmin <102chimee@gmail.com>
2023-11-22 00:50:53 +0900

    [#41] style : 파일 버튼 수정

--- src/components/Atom/image-input/index.js
+++ src/components/Atom/image-input/index.js
@@ -1,23 +1,28 @@
-// component/Atom/image-input/index.js
-
+// components/Atom/image-input/index.js
 import React, { useState } from "react";
-import { Container, ImageContainer, Text, Image, Input } from "./styled";
+import {
+  Container,
+  ImageContainer,
+  Text,
+  Image,
+  Input,
+  FileButton,
+  Label,
+} from "./styled";
 
 const AtomImageInput = ({ onUpdate, attribute }) => {
   const [selectedImage, setSelectedImage] = useState(null);
 
   const handleImageChange = (e) => {
-    const file = e.target.files[0]; // files(목록)중 첫번째 파일
-    setSelectedImage(file); // 선택된 파일을 상태에 저장
+    const file = e.target.files[0];
+    setSelectedImage(file);
 
     const reader = new FileReader();
     reader.onload = (event) => {
-      // event => 파일 읽는 이벤트, onload로 이벤트 핸들러 설정
       const base64String = event.target.result;
       onUpdate(attribute, base64String);
     };
 
-    // 파일을 읽고 Base64로 인코딩 => event 발생하면 위의 onload의 핸들러가 호출이된다.
     reader.readAsDataURL(file);
   };
 
@@ -28,7 +33,7 @@ const AtomImageInput = ({ onUpdate, attribute }) => {
       <ImageContainer>
         {selectedImage && (
           <Image
-            src={URL.createObjectURL(selectedImage)} // 이미지 미리보기
+            src={URL.createObjectURL(selectedImage)}
             alt="Selected"
             style={{ maxWidth: "300px" }}
           />
@@ -36,10 +41,15 @@ const AtomImageInput = ({ onUpdate, attribute }) => {
       </ImageContainer>
 
       {/* 파일 선택을 위한 파일 입력창 */}
+      <FileButton>
+        <Label htmlFor="fileInput">파일 선택</Label>
+      </FileButton>
       <Input
+        id="fileInput"
         type="file"
-        accept="image/*" // 이미지 파일만 선택 가능하도록 설정
+        accept="image/*"
         onChange={handleImageChange}
+        style={{ display: "none" }} // Hide the original file input
       />
     </Container>
   );
