76ec8c314c1d1e14c8a6a9ef65e76d1cca652fe3
안정민 <102chimee@gmail.com>
2023-11-22 13:15:04 +0900

    Issue 41 fix (#50)
    * [#41] fix : request body 추가
    
    * [#41] fix : api 데이터 수정

--- src/components/Molecule/balance/index.js
+++ src/components/Molecule/balance/index.js
@@ -5,30 +5,31 @@ const Balance = forwardRef((data, ref) => {
   const tableHeader = ["종목코드", "종목명", "투자수량", "평가금액"];
   console.log(data.data[0].transactionDtoList, "whdahr");
   const jusikData = data.data[0].transactionDtoList;
-  // const chaegwonData = data.data[1].transactionDtoList;
-  // const pasaengData = data.data[2].transactionDtoList;
-  const chaegwonData = [
-    {
-      code: "R6003492DB8",
-      name: "대한항공 105-2",
-      amount: "50",
-      price: "1000000",
-    },
-    {
-      code: "R6003492DB8",
-      name: "대한항공 105-2",
-      amount: "50",
-      price: "1000000",
-    },
-  ];
-  const pasaengData = [
-    {
-      code: "R6003492DB8",
-      name: "삼성인덱스프리미엄30증권투자회사",
-      amount: "50",
-      price: "1000000",
-    },
-  ];
+  const chaegwonData = data.data[1].transactionDtoList;
+  const pasaengData = data.data[2].transactionDtoList;
+  // const chaegwonData = [
+  //   {
+  //     code: "R6003492DB8",
+  //     name: "대한항공 105-2",
+  //     amount: "50",
+  //     price: "1000000",
+  //   },
+  //   {
+  //     code: "R6003492DB8",
+  //     name: "대한항공 105-2",
+  //     amount: "50",
+  //     price: "1000000",
+  //   },
+  // ];
+  // const pasaengData = [
+  //   {
+  //     code: "R6003492DB8",
+  //     name: "삼성인덱스프리미엄30증권투자회사",
+  //     amount: "50",
+  //     price: "1000000",
+  //   },
+  // ];
+
   return (
     <Container ref={ref}>
       <SubTitle subTitle="잔고/체결내역"></SubTitle>
